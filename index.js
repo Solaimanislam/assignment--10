5e333e851e029acadd5210732aece01d1b8bd589
Solaimanislam <islamsi2001@gmail.com>
2024-04-28 01:11:12 +0600

    added mycraft email id

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ysdrtdj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri);
+// console.log(uri);
 
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -38,6 +38,12 @@ async function run() {
         app.get('/craft', async(req, res) => {
             const cursor = craftCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/myCraft/:email', async( req, res) => {
+            console.log(req.params.email);
+            const result = await craftCollection.find({email: req.params.email}).toArray();
             res.send(result);
         })
 
