583a42ab8bdc7e58f26e37d7fab73a39b0414d14
Solaimanislam <islamsi2001@gmail.com>
2024-04-27 16:42:21 +0600

    adde get and post

--- index.js
+++ index.js
@@ -32,7 +32,22 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
 
+        const craftCollection = client.db('craftDB').collection('craft');
 
+
+        app.get('/craft', async(req, res) => {
+            const cursor = craftCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+
+        app.post('/craft', async(req, res) => {
+            const newCraft = req.body;
+            console.log(newCraft);
+            const result = await craftCollection.insertOne(newCraft);
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
